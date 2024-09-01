f4ec20638857e48f15d8140899fb4ee056c10f89
jeremy <102297631+proprophet2503@users.noreply.github.com>
2024-09-01 12:54:17 +0700

    Update Pr08.py

--- Pr08.py
+++ Pr08.py
@@ -8,6 +8,7 @@ population = int(input("Enter the community population :"))
 magnitude = 2 * totalflushed_day
 water_saved = waters_toflush*totalflushed_day - new_waters_toflush*totalflushed_day
 toilet_population_cost = newtoilet_cost * population
-print("The magnitude is", magnitude, "flush per day and the cost to build new toilet for the community is", toilet_population_cost," dollars.")
+print("The magnitude is", magnitude, "flush per day per toilet and the cost to build new toilet for the community is", toilet_population_cost," dollars.")
+print("The amount of water saved is", water_saved, "per day.")
 
 
